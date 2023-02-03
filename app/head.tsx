dfb6414f138179c74321d1eb70567772c2b31a85
Aditya Raj <raj@useplaza.com>
2023-02-03 11:14:05 +0530

    fix og tags

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,15 @@ export default function Head() {
       <title>Giftwise - Wise up your gifting game</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
+        property="og:title"
+        content="Giftwise - Wise up your gifting game"
+      />
+      <meta
+        property="og:description"
+        content="Discover thoughtful and personalized gift recommendations and say goodbye to gift-giving stress with your wise gift pal."
+      />
+      <meta property="og:image" content="./purple-giftwise-logo.png" />
+      <meta
         name="description"
         content="Discover thoughtful and personalized gift recommendations and say goodbye to gift-giving stress with your wise gift pal."
       />
